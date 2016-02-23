ae6216fd34f35a6c06d600866535652a1ab55194
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-02-23 09:46:06 -0500

    Añadiendo express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+app = express(),
+http = require('http'),
+server = http.createServer(app),
+port = process.env.PORT || 8000
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.static('public'))
+
+server.listen(port, () => {console.log("Server listen in " + port)})
