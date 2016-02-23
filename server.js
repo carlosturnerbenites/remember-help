95f469116fb566b7d23255a8981d5b907c1afce3
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-02-23 09:47:16 -0500

    Añadiendo endpoints basicos y sus respectivas views

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express"),
 app = express(),
 http = require('http'),
 server = http.createServer(app),
-port = process.env.PORT || 8000
+port = process.env.PORT || 8000,
+urlGeneral = require('./urls/general')
+
+app.use("",urlGeneral)
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.static('public'))
+
 
 server.listen(port, () => {console.log("Server listen in " + port)})
