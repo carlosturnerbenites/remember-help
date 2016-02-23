6fe2bc4bb3a016235ab96467e694c28997b6bdd8
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-02-23 09:52:47 -0500

    Maquetacion de actividades y Mensajes

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const gulp = require('gulp'),
 	notify = require('gulp-notify'),
 	stylus = require('gulp-stylus'),
 	nib = require('nib'),
+	plumber = require('gulp-plumber'),
 	config = require('./config/configDev.json')
 
 function compileStyl() {
 	gulp
 	.src(config.gulp.srcStyle)
+	.pipe(plumber())
 	.pipe(stylus({use: nib(),compress: true}))
 	.pipe(gulp.dest(config.gulp.dest))
 	.pipe(notify({title : 'Stylus',message: 'Compile Stylus'}))
--- urls/children.js
+++ urls/children.js
@@ -1,8 +1,8 @@
 const express = require('express'),
 	router = express.Router()
 
-router.get('/activities',(req,res) => res.render('children/activities'))
-router.get('/messages',(req,res) => res.render('children/messages'))
-router.get('/reminders',(req,res) => res.render('children/reminders'))
+router.get('/activities',(req,res) => res.render('children/activities',{classcss:"reminders"}))
+router.get('/messages',(req,res) => res.render('children/messages',{classcss:"messages"}))
+router.get('/reminders',(req,res) => res.render('children/reminders',{classcss:"activities"}))
 
 module.exports = router
