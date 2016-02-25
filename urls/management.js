9168fca95c98d7ade43d947efb5e098ae37e3572
carlosturnerbenites <carlosturnerbenites@gmail.com>
2016-02-25 17:42:17 -0500

    Estilizacion de vistas de managements

--- urls/management.js
+++ urls/management.js
@@ -1,7 +1,11 @@
 const express = require('express'),
-	router = express.Router()
+	router = express.Router(),
+	models = require('./../models/'),
+	utils = require("./../utils")
+var stylesPage = ['styleRed','styleYellow','styleBlue','styleGreen','styleOscure']
 
-router.get('/statistics',(req,res) => res.render('management/statistics'))
-router.get('/line-evolution',(req,res) => res.render('management/lineEvolution'))
+router.get('/statistics',(req,res) => res.render('management/statistics',{classcss:stylesPage.getRandom()}))
+router.get('/line-evolution',(req,res) => res.render('management/lineEvolution',{classcss:stylesPage.getRandom()}))
+router.get('/collections',(req,res) => res.render('collections/index',{classcss:stylesPage.getRandom()}))
 
 module.exports = router
---
+++ utils/index.js
@@ -0,0 +1,3 @@
+Array.prototype.getRandom = function (){
+	return this[Math.floor(Math.random()*this.length)]
+}
